6d34a0ec86964a944a75ce74fcbe3969d892097d
Azhong5252 <chenzhizhong.0925@gmail.com>
2024-09-20 00:07:53 +0800

    TTT

---
+++ download_model.bat
@@ -0,0 +1,3 @@
+@echo off
+call tar -xf "C:\Users\user\Desktop\AI_entity_robot\pretrained_models.zip" -C "C:\Users\user\Desktop\AI_entity_robot\GPT-SoVITS-main\GPT_SoVITS"
+call tar -xf "C:\Users\user\Desktop\AI_entity_robot\models.zip" -C "C:\Users\user\Desktop\AI_entity_robot\GPT-SoVITS-main\tools\asr"

1497fa68482a846cf0f5b2121e6201c67d61b17f
Azhong5252 <chenzhizhong.0925@gmail.com>
2024-09-21 23:17:33 +0800

    TTT

--- test_models.bat
+++ test_models.bat
@@ -1,5 +1,5 @@
 @echo off
 call curl -L -o GPT-SoVITS-main\Ayaka_sovits_models.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_test_models/Ayaka_sovits_models.zip
-call curl -L -o GPT-SoVITS-main\logs.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_test_models/Alogs.zip
+call curl -L -o GPT-SoVITS-main\logs.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_test_models_log/Alogs.zip
 call tar -xf "GPT-SoVITS-main\Ayaka_sovits_models.zip" -C "GPT-SoVITS-main"
 call tar -xf "GPT-SoVITS-main\logs.zip" -C "GPT-SoVITS-main"

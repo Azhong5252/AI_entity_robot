92640b3c4f0cdb64410d8190bd41487c149d8aa8
Azhong5252 <163645015+Azhong5252@users.noreply.github.com>
2024-09-24 01:49:02 +0800

    Update 2自動下載且解壓模型.bat

--- 2自動下載且解壓模型.bat
+++ 2自動下載且解壓模型.bat
@@ -1,5 +1,5 @@
 @echo off
-curl -L -o GPT-SoVITS-main\pretrained_models.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_uvr5/uvr5.zip
+curl -L -o GPT-SoVITS-main\uvr5.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_uvr5/uvr5.zip
 curl -L -o GPT-SoVITS-main\pretrained_models.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_pretrained_models/pretrained_models.zip
 curl -L -o GPT-SoVITS-main\models.zip https://github.com/Azhong5252/AI_entity_robot/releases/download/GPT-SoVITS_ASR_model/models.zip
 tar -xf "GPT-SoVITS-main\uvr5.zip" -C "GPT-SoVITS-main\tools\uvr5\uvr5_weights"
